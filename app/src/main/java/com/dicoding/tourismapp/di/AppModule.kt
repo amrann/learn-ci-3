e5956de75cb61dc32815f7bdcbec51eb954e8c6b
meran <amr.bin.an@gmail.com>
2024-12-05 06:26:15 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/tourismapp/di/AppModule.kt
@@ -0,0 +1,19 @@
+package com.dicoding.tourismapp.di
+
+import com.dicoding.tourismapp.core.domain.usecase.TourismInteractor
+import com.dicoding.tourismapp.core.domain.usecase.TourismUseCase
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class AppModule {
+
+    @Binds
+    @Singleton
+    abstract fun provideTourismUseCase(tourismInteractor: TourismInteractor): TourismUseCase
+
+}
